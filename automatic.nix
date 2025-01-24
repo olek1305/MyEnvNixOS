5e40ab7aaf706ebab1ddee468a5052fb8d47f3ad
Arcylisz <installer@gmail.com>
2025-01-24 13:07:11 +0100

    forgot add ;

--- automatic.nix
+++ automatic.nix
@@ -3,11 +3,12 @@
 {
     # Automatic updating
     system.autoUpgrade.enable = true;
-    system.autoUpgrade.dates = "monthly"
+    system.autoUpgrade.dates = "monthly";
 
     # Automatic cleanup
-    nix.gc.automatic = true;
-    nix.gc.dates = "weekly";
-    nix.gc.options = "--delete-older-than 7d";
-    nix.settings.auto-optimise-store = true;
+    nix.gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+    };
 }
