19a0e8a369cb36b1ac0298f773652cc9db285874
olek1305 <olek1305@gmail.com>
2025-03-23 11:40:54 +0100

    run nvtop as root

--- packages-to-install.nix
+++ packages-to-install.nix
@@ -1,5 +1,32 @@
 { config, pkgs, ... }:
 
+let
+  customNvtop = pkgs.nvtop.overrideAttrs (oldAttrs: {
+    version = "3.1.0+2025-03-22-git";
+    src = pkgs.fetchFromGitHub {
+      owner = "Syllo";
+      repo = "nvtop";
+      rev = "ca52d04f2e69ea945f67b93741a8530e502021b3";
+      hash = "sha256-MQJDs5fee8uRqgaIF1lFcph+vNECyY4MBcK618zaisg=";
+    };
+    
+    # Add libdrm to buildInputs
+    buildInputs = oldAttrs.buildInputs ++ [pkgs.libdrm];
+    
+    # Make sure env.NIX_CFLAGS_COMPILE includes libdrm headers
+    env = (oldAttrs.env or {}) // {
+      NIX_CFLAGS_COMPILE = (oldAttrs.env.NIX_CFLAGS_COMPILE or "") + 
+      " -isystem ${pkgs.lib.getDev pkgs.libdrm}/include/libdrm";
+    };
+    
+    postFixup = ''
+      ${oldAttrs.postFixup or ""}
+      patchelf \
+        --set-rpath "$(patchelf --print-rpath $out/bin/nvtop):${pkgs.lib.makeLibraryPath [pkgs.libdrm]}" \
+        $out/bin/nvtop
+    '';
+  });
+in
 {
     environment.systemPackages = with pkgs; [
         alacritty
@@ -27,15 +54,12 @@
         mesa-demos
         nodejs_23
         ncurses
-        nvtop
+        customNvtop
         oh-my-zsh
         pciutils
-        #php # 8.3.15
-        #php83Packages.cyclonedx-php-composer
         (php82.withExtensions (exts: with pkgs.phpExtensions; [ mongodb ]))
         php82
         pkg-config
-        # php82Packages.cyclonedx-php-composer
         steam
         symfony-cli
         systemd
@@ -53,48 +77,12 @@
     nixpkgs.config.allowUnfree = true;
 
     programs.firefox.enable = true;
-
-    nixpkgs.overlays = [
-        (final: prev: {
-            nvtopPackages = 
-            let 
-                nvtopOverride = oldAttrs: oldAttrs // {
-                    version = "3.1.0+2025-03-22-git";
-                    src = prev.fetchFromGitHub {
-                        owner = "Syllo";
-                        repo = "nvtop";
-                        rev = "ca52d04f2e69ea945f67b93741a8530e502021b3";
-                        hash = "sha256-MQJDs5fee8uRqgaIF1lFcph+vNECyY4MBcK618zaisg=";
-                    };
-                    
-                    # Add libdrm to buildInputs regardless of needDrm
-                    buildInputs = oldAttrs.buildInputs ++ [prev.libdrm];
-                    
-                    # Make sure env.NIX_CFLAGS_COMPILE includes libdrm headers
-                    env = (oldAttrs.env or {}) // {
-                        NIX_CFLAGS_COMPILE = (oldAttrs.env.NIX_CFLAGS_COMPILE or "") + 
-                        " -isystem ${prev.lib.getDev prev.libdrm}/include/libdrm";
-                    };
-                    
-                    # Update postFixup to include libdrm in the rpath
-                    # Using a simpler approach that doesn't rely on if condition
-                    postFixup = ''
-                        ${oldAttrs.postFixup or ""}
-                        patchelf \
-                            --set-rpath "$(patchelf --print-rpath $out/bin/nvtop):${prev.lib.makeLibraryPath [prev.libdrm]}" \
-                            $out/bin/nvtop
-                    '';
-                };
-            in 
-            builtins.mapAttrs (name: pkg: 
-                pkg.overrideAttrs nvtopOverride
-            ) prev.nvtopPackages;
-            
-            # Also update the main nvtop package
-            nvtop = final.nvtopPackages.full;
-        })
-    ];
+    
+    # Grant the necessary permissions or run nvtop as root.
+    security.wrappers.nvtop = {
+        owner = "root";
+        group = "root";
+        capabilities = "cap_perfmon+ep";
+        source = "${customNvtop}/bin/nvtop";
+    };
 }
-
-# command to install packages
-# nix-shell -p neovim
